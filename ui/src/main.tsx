52b8837573fab23f5140e33cf95484497fc6cc24
Erik Marques <santanaerik1106@gmail.com>
2026-09-01 12:29:57 -0300

    ui - first commit

---
+++ ui/src/main.tsx
@@ -0,0 +1,30 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { routeTree } from './routeTree.gen';
+
+import './global.css';
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root not found!');
+}
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+}
